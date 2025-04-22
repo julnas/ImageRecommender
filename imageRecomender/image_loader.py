b894171a5a549b371e50b5c7c6fdd9c222f988ad
BelizSnl <belizsenol@gmail.com>
2025-04-22 15:52:45 +0200

    image_loader implement classes and methosds

--- imageRecomender/image_loader.py
+++ imageRecomender/image_loader.py
@@ -1,1 +1,22 @@
-#a generator that loads the images
+class ImageLoader:
+    def __init__(self, db, base_dir: str):
+        """
+        Loads images from disk based on paths stored in the database.
+
+        Parameters:
+        db: an instance of DatabaseManager
+        base_dir: root path to the external image folder (e.g. '/media/usbdrive/images/')
+        """
+        self.db = db
+        self.base_dir = base_dir
+
+    def load_image(self, image_id):
+        """
+        Loads an image by ID using the database and base_dir.
+        """
+
+
+    def image_generator(self):
+        """
+        Generator that yields for each valid image.
+        """
