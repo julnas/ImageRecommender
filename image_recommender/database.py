eee7367093882ebb6644aa80c476ea92fe727c3a
BelizSnl <belizsenol@gmail.com>
2025-04-25 18:08:06 +0200

    cleaned database.py

--- image_recommender/database.py
+++ image_recommender/database.py
@@ -12,32 +12,6 @@ class Database:
         self.connection = sqlite3.connect(db_path)  #connect to the SQLite DB file  -> SQLite safes how i can find a pic and the things i know about the  pic
         self.cursor = self.connection.cursor()       #create a cursor to execute SQL commands
 
-    def init_db(self):
-        """
-        Creates the 'images' table if it doesn't exist already.
-        """
-        self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS images (
-                image_id INTEGER PRIMARY KEY,
-                file_path TEXT NOT NULL
-            );
-        """)
-        self.connection.commit()  #save changes
-
-    def insert_image(self, image_id: int, file_path: str):
-        """
-        Inserts a new image entry into the database.
-
-        Parameters:
-        image_id: unique numeric ID
-        file_path: path to the image file (relative to the base folder)
-        """
-        self.cursor.execute("""
-            INSERT INTO images (image_id, file_path)
-            VALUES (?, ?)
-        """, (image_id, file_path))
-        self.connection.commit()  #save the insertion
-
     def get_image_path(self, image_id: int):
         """
         Retrieves the file path of an image based on its image ID.
